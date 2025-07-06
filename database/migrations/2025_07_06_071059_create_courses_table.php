5e54df8c2c9b8a157310c16cad38567b3a0ee040
verena.vynne@gmail.com <verena.vynne@gmail.com>
2025-07-06 17:04:40 +0700

    feat: add migration and model modified

--- database/migrations/2025_07_06_071059_create_courses_table.php
+++ database/migrations/2025_07_06_071059_create_courses_table.php
@@ -17,6 +17,11 @@ return new class extends Migration
             $table->text('courseText');
             $table->string('coursePicture')->nullable();
             $table->enum('courseLevel',['dasar', 'menengah', 'lanjutan']);
+            $table->enum('courseType',['lukis','tari','musik','fotografi']);
+            $table->enum('coursePaymentType',['gratis','berbayar'])->nullable();
+            $table->integer('courseDurationInMinutes')->nullable();
+            $table->float('courseReview')->nullable();
+            $table->boolean('bookmarked')->nullable();
             $table->timestamps();
         });
     }
